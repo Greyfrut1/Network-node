1f1c38409c50dce8285a7e6fa56717d47ffbd508
Greyfrut <greyfrut@wishdesk.com>
2024-10-28 11:10:49 +0200

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import { errorHandler } from './middlewares/errorMiddleware';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 
 app.use(express.json()); // Парсинг JSON
 
