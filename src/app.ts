081ccc8f0405e5ddd89b267d062fb711b28bc799
Greyfrut <greyfrut@wishdesk.com>
2024-11-07 09:40:21 +0200

    Cors front url

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: process.env.FRONT_URL || 'http://localhost:3000' }));
 
 
 app.use(express.json()); // Парсинг JSON
